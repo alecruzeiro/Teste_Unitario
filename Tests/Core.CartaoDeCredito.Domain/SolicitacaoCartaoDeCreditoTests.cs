6e9a428f9cf80340b7c01960f6a7cdc025718b18
Alexandre Cruzeiro <alexandre.cruzeiro@protonmail.com>
2021-04-27 00:16:09 -0300

    Solicitar cartão com dados válidos -- RED v2

---
+++ Tests/Core.CartaoDeCredito.Domain/SolicitacaoCartaoDeCreditoTests.cs
@@ -0,0 +1,25 @@
+﻿using Core.CartaoDeCredito.Domain;
+using Xunit;
+
+namespace Tests.CartaoDeCredito.Domain
+{
+    public class SolicitacaoCartaoDeCreditoTests
+    {
+        [Fact(DisplayName = "Solicitar cartão com dados válidos")]
+        [Trait("Categoria", "Cartão de Crédito - Solicitação")]
+        public void CartaoDeCredito_SolicitarCartaoComDadosValidos_DeveCriarSolicitacaoComSucesso()
+        {
+            //Arrange
+            var solicitacaoCartaoDeCredito = new SolicitacaoCartaoDeCredito("Teste Teste", 
+                "01234567890", 
+                "1234567890", 
+                "Analista de Sistemas", 
+                2000m,
+                "Teste Plástico");
+
+            //Act, Assert
+            Assert.True(solicitacaoCartaoDeCredito.validationResult.IsValid);
+        }
+
+    }
+}
---
+++ src/Core/SolicitacaoCartaoDeCredito.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+
+namespace Core.CartaoDeCredito.Domain
+{
+    public class SolicitacaoCartaoDeCredito
+    {
+        public readonly ValidationResult validationResult;
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Profissao { get; set; }
+        public decimal Renda { get; set; }
+        public string NomeNoCartao { get; set; }
+
+
+        public SolicitacaoCartaoDeCredito(string nome, 
+            string cpf, 
+            string rg, 
+            string profissao, 
+            decimal renda, 
+            string nomeNoCartao)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Rg = rg;
+            Profissao = profissao;
+            Renda = renda;
+            NomeNoCartao = nomeNoCartao;
+
+            validationResult = new SolicitacaoCartaoDeCreditoValidation().Validate(this);
+        }
+    }
+
+    internal class SolicitacaoCartaoDeCreditoValidation : AbstractValidator<SolicitacaoCartaoDeCredito>
+    {
+
+
+        public SolicitacaoCartaoDeCreditoValidation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
