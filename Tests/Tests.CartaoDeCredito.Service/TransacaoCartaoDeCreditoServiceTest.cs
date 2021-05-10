9075a0aaa66ad3d5e0a469c3560ae87b180a954b
Alexandre Cruzeiro <alexandre.cruzeiro@protonmail.com>
2021-05-10 19:51:31 -0300

    Se os dados do cartão forem inválidos, deve retornar mensagem de erro -- RED

--- Tests/Tests.CartaoDeCredito.Service/TransacaoCartaoDeCreditoServiceTest.cs
+++ Tests/Tests.CartaoDeCredito.Service/TransacaoCartaoDeCreditoServiceTest.cs
@@ -49,5 +49,33 @@ namespace Tests.CartaoDeCredito.Service
             _transacaoCartaoDeCreditoRepository.Verify(s => s.Criar(It.IsAny<TransacaoCartaoDeCredito>()), Times.Once);
             Assert.True(transacaoResponse.TransacaoRealizadaComSucesso);
         }
+
+        [Fact(DisplayName = "Se os dados do cartão forem inválidos, deve retornar mensagem de erro")]
+        [Trait("Categoria", "Cartão de Crédito - Transação")]
+        public void TransacaoCartaoDeCredito_TransacaoComDadosInvalidosNoCartao_NaoDeveTransacionar()
+        {
+            //Arrange
+            var transacao = new TransacaoCartaoDeCreditoRequest()
+            {
+                CartaoDeCredito = new CartaoDeCreditoRequest()
+                {
+                    Cpf = "01234567890",
+                    Cvv = "123",
+                    NumeroCartaoVirtual = "1234567890",
+                    DataDeValidade = "12/2026",
+                    NomeNoCartao = "Teste do Teste"
+                },
+                ValorTotal = 100m
+            };
+
+            _transacaoCartaoDeCreditoRepository.Setup(c => c.Criar(It.IsAny<TransacaoCartaoDeCredito>()))
+                                            .Returns(true);
+            //Act
+            var transacaoResponse = _transacaoCartaoDeCreditoService.Criar(transacao);
+
+            //Assert
+            _transacaoCartaoDeCreditoRepository.Verify(s => s.Criar(It.IsAny<TransacaoCartaoDeCredito>()), Times.Never);
+            Assert.False(transacaoResponse.TransacaoRealizadaComSucesso);
+        }
     }
 }
