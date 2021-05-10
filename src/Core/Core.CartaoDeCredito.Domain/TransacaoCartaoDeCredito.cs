9295881cfc84fc89f8028ada26bb3245e0b592f2
Alexandre Cruzeiro <alexandre.cruzeiro@protonmail.com>
2021-05-10 19:21:59 -0300

    Ao fazer uma transação em lojas virtuais, deve ser informado Nome no cartão, Número do cartão virtual, CVV, data de validade, CPF e valor total da transação -- GREEN

--- src/Core/Core.CartaoDeCredito.Domain/TransacaoCartaoDeCredito.cs
+++ src/Core/Core.CartaoDeCredito.Domain/TransacaoCartaoDeCredito.cs
@@ -22,7 +22,7 @@ namespace Core.CartaoDeCredito.Domain
     {
         public TransacaoCartaoDeCreditoValidator()
         {
-            throw new System.Exception();
+            
         }
     }
 }
